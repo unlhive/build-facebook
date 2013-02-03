549df43f698f087891b1bdbee1839c7126b46e26
UNL HIVE <hive.unl@gmail.com>
2013-02-03 09:47:18 -0500

    phase 10: but what if the person knows already the url - then they can hack in?; but before that, lets fix the template problem. login.php looks terrible. create header.php, footer.php - use it in index.php, login.php, and some random-file.php; fix css on login page

---
+++ team0a/phase10/_includes/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<?php
+	include_once('db-connection.php');
+?>
+<html>
+<head>
+    <link href="_css/reset.css" style="type/css" rel="stylesheet">
+    <link href="_css/base.css" style="type/css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Julius+Sans+One' rel='stylesheet' type='text/css'>
+    <Title>Coding Bootcamp</Title>
+</head>
+
+<body>
+    <div id="top-banner">
+        <div id="top-banner-container">
+            <p>facebook</p>
+            
+        </div>
+    </div>
+    <div id="content">
+        <div id="content-container">
+        
+        
---
+++ team0a/phase10/check-login.php
@@ -0,0 +1,27 @@
+<?php
+ob_start();
+
+if(isset($_POST['username']) && strlen($_POST['username'])!=0){
+	$username = $_POST['username'];
+} else {
+	header('Location: login.php');
+}
+
+if(isset($_POST['password']) && strlen($_POST['password'])!=0){
+	$password = $_POST['password'];
+} else {
+	// go back to login page
+	header('Location: login.php');
+}
+
+if($username == 'sourabh' && $password == 'hive'){
+	
+	header('Location: index.php');
+} else {
+	
+	// then go back to login page
+	header('Location: login.php');
+}
+
+ob_flush();
+?>
